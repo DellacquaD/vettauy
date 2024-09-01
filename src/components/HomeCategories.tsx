c553e545583d92592c973d62ca3b12be822155da
DellacquaD <d.dellacquaruiz@gmail.com>
2024-09-01 17:35:02 -0300

    home page

---
+++ src/components/HomeCategories.tsx
@@ -0,0 +1,42 @@
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+export default function TitlebarImageList() {
+  return (
+    <ImageList style={{ gap: '1rem', padding: '2rem' }}>
+      {itemData.map((item) => (
+        <ImageListItem key={item.img} >
+          <img
+            srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            src={`${item.img}?w=248&fit=crop&auto=format`}
+            alt={item.title}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={item.title}
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+const itemData = [
+  {
+    img: 'https://images.unsplash.com/photo-1721222204755-669d8056cdb4?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Mobiliario',
+  },
+  {
+    img: 'https://plus.unsplash.com/premium_photo-1684980181655-ab1cf2e31b17?q=80&w=2083&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    title: 'Kids',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1495130656884-992a9f30d178?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8dXRlbnNpbGxvcyUyMG1hZGVyYXxlbnwwfHwwfHx8MA%3D%3D',
+    title: 'Home',
+  },
+  {
+    img: 'https://aluminiossardina.es/wp-content/uploads/2021/09/cerramiento-ventanas-abatibles.jpg',
+    title: 'Vidriado',
+  },
+];
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,9 @@
+import HomeCategories from '../components/HomeCategories'
+
 function Home() {
     return (
         <div style={{ border: "1px solid black", marginTop: '12vh', minHeight: '80vh', backgroundImage: "url(https://editorialtelevisa.brightspotcdn.com/dims4/default/16ac18c/2147483647/strip/false/crop/1500x857+0+0/resize/1486x849!/quality/90/?url=https%3A%2F%2Fk2-prod-editorial-televisa.s3.us-east-1.amazonaws.com%2Fbrightspot%2Fc2%2F3e%2F22fe65ae49adbffd7cc78671679d%2Fapartamento-moderno-lujo-comoda-decoracion-almohadas-generada-ia.jpg)", backgroundRepeat: 'round', backgroundSize: 'cover' }}>
-            {/* <h1>Este es el home</h1> */}
+            <HomeCategories />
         </div>
     )
 }
